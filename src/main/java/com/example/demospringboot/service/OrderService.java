e0ed0051f62c6ea4f6e7a44ef15205a1e3f27c30
naruhin <dimonkoko1@gmail.com>
2021-09-15 19:38:46 +0300

    find by id method

--- src/main/java/com/example/demospringboot/service/OrderService.java
+++ src/main/java/com/example/demospringboot/service/OrderService.java
@@ -8,5 +8,13 @@ public interface OrderService {
 
     Order saveOrder(Order order);
 
+    Order getOrderById(Long id);
+
     List<Order> getAllOrders();
+
+    Order updateOrder(final Order order);
+
+    void removeOrderById(final Long id);
+
+    void removeAllOrders();
 }
