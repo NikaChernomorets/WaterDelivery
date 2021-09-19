83b52a8fcf16dc19de1c6002531d1fdfba5e7afe
Aleksandr <zholud.aleksandr@gmail.com>
2021-09-20 00:15:56 +0300

    include Lombok in Order Class

--- src/main/java/waterDelivery/domain/Order.java
+++ src/main/java/waterDelivery/domain/Order.java
@@ -1,97 +1,33 @@
 package waterDelivery.domain;
 
 import javax.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 @Entity
-@Table(name = "ORDERS")
-public class Order {
-
+@Table( name = "ORDERS" )
+public class Order
+{
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue( strategy = GenerationType.SEQUENCE )
     private long id;
-
+    
     private String name;
-
+    
     private String startDate;
-
+    
     private String EndDate;
-
-    public long getId()
-    {
-        return id;
-    }
-
-    public void setId(long id)
-    {
-        this.id = id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public String getStartDate()
-    {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate)
-    {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate()
-    {
-        return EndDate;
-    }
-
-    public void setEndDate(String endDate)
-    {
-        EndDate = endDate;
-    }
-
-    public String getStatus()
-    {
-        return status;
-    }
-
-    public void setStatus(String status)
-    {
-        this.status = status;
-    }
-
-    public String getCost()
-    {
-        return cost;
-    }
-
-    public void setCost(String cost)
-    {
-        this.cost = cost;
-    }
-
-    public Customer getCustomer()
-    {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer)
-    {
-        this.customer = customer;
-    }
-
+    
     private String status;
-
+    
     private String cost;
-
-    @OneToOne(mappedBy = "order")
-    private Customer customer;
+    
+    @OneToOne( mappedBy = "order" ) private Customer customer;
+    
+    public Order() { }
 }
 
 
