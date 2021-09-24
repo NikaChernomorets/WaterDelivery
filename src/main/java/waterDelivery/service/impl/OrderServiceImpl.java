cdd9d70c2210176afd6347b45d3105a1458ce4b8
Dmytry <85928123+romanov-create@users.noreply.github.com>
2021-09-25 00:32:19 +0300

    -add  logger for Order -add lombok.var -add method updateOrder

--- src/main/java/waterDelivery/service/impl/OrderServiceImpl.java
+++ src/main/java/waterDelivery/service/impl/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package waterDelivery.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.var;
 import org.springframework.stereotype.Service;
 import waterDelivery.domain.Order;
 import waterDelivery.exception.NoDataFoundException;
@@ -7,9 +9,11 @@ import waterDelivery.exception.OrderNotFoundException;
 import waterDelivery.repository.OrderRepository;
 import waterDelivery.service.OrderService;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
+@Slf4j
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
 
@@ -19,11 +23,15 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order saveOrder(Order order) {
-        return orderRepository.save(order);
+        log.info("saveOrder() - start: order = {}", order);
+        var savedOrder = orderRepository.save(order);
+        log.info("saveOrder() - end: order = {}", savedOrder.getId());
+        return savedOrder;
     }
 
     @Override
     public Order getOrderById(Long id) {
+        log.info("orderRepository.findById() - start");
         return orderRepository.findById(id)
                 .orElseThrow(() -> new OrderNotFoundException(id));
     }
@@ -32,25 +40,39 @@ public class OrderServiceImpl implements OrderService {
     public List<Order> getAllOrders() {
 
         List<Order> orders = orderRepository.findAll();
+        log.info("orderRepository.findAll() - start");
         if (orders.isEmpty()) {
+            log.error("orderRepository.findAll() - exception");
             throw new NoDataFoundException();
         }
-        return orderRepository.findAll();
+        var allOrders = orderRepository.findAll();
+        log.info("orderRepository.findAll() - end");
+        return allOrders;
     }
 
     @Override
     public Order updateOrder(Order order) {
-        return null;
+        log.info("updateOrder - start");
+        return orderRepository.findById(order.getId())
+                .map(entity -> {
+                    entity.setName(order.getName());
+                    entity.setCost(order.getCost());
+                    entity.setStatus(order.getStatus());
+                    return orderRepository.save(entity);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("Order not found with id = " + order.getId()));
     }
 
     @Override
     public void removeOrderById(Long id) {
+        log.info("removeOrderById - succeed");
         orderRepository.delete(orderRepository.findById(id)
                 .orElseThrow(() -> new OrderNotFoundException(id)));
     }
 
     @Override
     public void removeAllOrders() {
+        log.info("removeAllOrders - succeed");
         orderRepository.deleteAll();
     }
 
