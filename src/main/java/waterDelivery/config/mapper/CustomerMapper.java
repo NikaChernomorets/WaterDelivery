eadaaee40a1a58c5add852f57882e86abecb7786
Sn1ckey <82238466+Sn1ckey@users.noreply.github.com>
2021-09-27 19:13:44 +0300

    added new methods for DTO

--- src/main/java/waterDelivery/config/mapper/CustomerMapper.java
+++ src/main/java/waterDelivery/config/mapper/CustomerMapper.java
@@ -3,18 +3,26 @@ package waterDelivery.config.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import waterDelivery.domain.Customer;
-import waterDelivery.dto.CustomerDTO;
+import waterDelivery.dto.customerDTO.CustomerDeleteDTO;
+import waterDelivery.dto.customerDTO.CustomerReadDTO;
+import waterDelivery.dto.customerDTO.CustomerCreateDTO;
+import waterDelivery.dto.customerDTO.CustomerUpdateDTO;
 
-import java.util.Collection;
 
 @Mapper (componentModel = "spring")
 public interface CustomerMapper
 {
     CustomerMapper INSTANCE = Mappers.getMapper( CustomerMapper.class );
     
-    CustomerDTO toDto( Customer customer );
-    
-    Customer toCustomer( CustomerDTO customerDTO );
-    
-    Collection <CustomerDTO> listToDto( Collection <Customer> list );
+    CustomerReadDTO toReadDto(Customer customer );
+    CustomerCreateDTO toSaveDto(Customer customer );
+    CustomerUpdateDTO toUpdateDto(Customer customer );
+    CustomerDeleteDTO toDeleteDto(Customer customer );
+
+    Customer toSaveCustomer(CustomerCreateDTO customerSaveDTO);
+    Customer toReadCustomer(CustomerReadDTO customerReadDTO);
+    Customer toUpdateCustomer(CustomerUpdateDTO customerUpdateDTO);
+    Customer toDeleteCustomer(CustomerDeleteDTO customerDeleteDTO);
+
+
 }
