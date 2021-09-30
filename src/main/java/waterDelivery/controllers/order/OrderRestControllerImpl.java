e4da3c3fdef7f6c92437fff26d063d1656cb633d
naruhin <dimonkoko1@gmail.com>
2021-09-30 12:30:00 +0300

    some fixes and refactoring

--- src/main/java/waterDelivery/controllers/order/OrderRestControllerImpl.java
+++ src/main/java/waterDelivery/controllers/order/OrderRestControllerImpl.java
@@ -1,15 +1,13 @@
 package waterDelivery.controllers.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import waterDelivery.config.mapper.OrderMapper;
 import waterDelivery.domain.Order;
-import waterDelivery.dto.orderDTO.OrderReadDTO;
 import waterDelivery.dto.orderDTO.OrderCreateDTO;
+import waterDelivery.dto.orderDTO.OrderReadDTO;
+import waterDelivery.dto.orderDTO.OrderUpdateDTO;
 import waterDelivery.service.OrderService;
 
 import java.util.List;
@@ -18,24 +16,42 @@ import java.util.List;
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderRestControllerImpl implements OrderRestController{
 
-    @Autowired
-    private OrderService orderService;
+    private final OrderService orderService;
+
+    public OrderRestControllerImpl(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @Override
+    @GetMapping("/orders")
+    @ResponseStatus(HttpStatus.OK)
     public List<Order> getAll() {
         return orderService.getAllOrders();
     }
 
     @Override
+    @GetMapping("/orders/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public OrderReadDTO getById(@PathVariable long id) {
         Order order = orderService.getOrderById(id);
         return OrderMapper.orderINSTANCE.toOrderReadDTO(order);
     }
 
-   /* @Override
-    public OrderCreateDTO saveOrder(@RequestBody OrderCreateDTO requestForSave) {
+    @Override
+    @PostMapping("/orders")
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderReadDTO saveOrder(@RequestBody OrderCreateDTO requestForSave) {
         Order order = OrderMapper.orderINSTANCE.toSaveOrder(requestForSave);
-        return OrderMapper.orderINSTANCE.toOrderSaveDTO(orderService.saveOrder(order));
-    }*/
+        return OrderMapper.orderINSTANCE.toOrderReadDTO(orderService.saveOrder(order));
+    }
+
+    @Override
+    @PutMapping("/orders/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public OrderReadDTO updateOrder(@PathVariable("id") long id, @RequestBody OrderUpdateDTO requestForUpdate) {
+        Order order = OrderMapper.orderINSTANCE.toUpdateOrder(requestForUpdate);
+        orderService.updateOrder(id, order);
+        return OrderMapper.orderINSTANCE.toOrderReadDTO(order);
+    }
 
 }
